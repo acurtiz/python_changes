8f096ce839f59e1a613b8c84ca593b85d360dc7e
Ricky Mutschlechner <mutschlechne@wisc.edu>
2016-12-20 21:36:45 -0600

    changing lambda func code, will not run anymore though as I changed the builtins to look good for presentation.

--- shared.py
+++ shared.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 import os
 import time
-import blah
+import lambda_func
 
 def Checkpoint():
-    blah._START_FREEZE_ = 1
+    blah._CHECKPOINT_ = 1
 def Restore():
-    blah._SPECIAL_ = 1
+    blah._RESTORE_ = 1
 
 start = time.time()
 for i in range(1000):
     Checkpoint()
-    blah.f()
+    lambda_func.f()
     Restore()
 end = time.time()
 
